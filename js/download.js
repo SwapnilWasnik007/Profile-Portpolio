c7c68612a08392f567ca446c2b09d09109ceb28d
spnilwasnik007 <swapnil.wasnik87@gmail.com>
2023-05-24 02:22:13 +0530

    Added new resume link

--- js/download.js
+++ js/download.js
@@ -1,5 +1,5 @@
 function downloadResumeFromGDrive () {
-    let url = "https://drive.google.com/file/d/12o69VGBFxKLP8Bp448oG-RE4kIz1neKA/view?usp=sharing";
+    let url = "https://drive.google.com/file/d/12o69VGBFxKLP8Bp448oG-RE4kIz1neKA/view?usp=drive_link";
     fetch(url).then(function(t) {
         return t.blob().then((b)=>{
             var a = document.createElement("a");
